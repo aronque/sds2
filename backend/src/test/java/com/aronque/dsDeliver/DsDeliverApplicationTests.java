732c11e277749d5395b1a0f3f294ea2549a7e76a
Vitor Siwerski Aronque <vitorsiwerskiaronque@gmail.com>
2021-01-05 20:14:25 -0300

    Project Created

---
+++ backend/src/test/java/com/aronque/dsDeliver/DsDeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aronque.dsDeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsDeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
